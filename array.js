6ab54a4e4ebbb804555bd1cab6e18fb57d922737
Jaron Esquivel <jaron.esq@gmail.com>
2018-11-14 15:43:13 -0700

    array

---
+++ array.js
@@ -0,0 +1,42 @@
+// comment in js
+// create an array
+// create an empty array
+var myArr = new Array()
+// recommended
+var myArr2 = []
+
+// create an array of a certain size
+var myBigArr = new Array(100)
+var myBigArr2 = [,,]
+var myBigArr2 = [1,2,3,4]
+
+var array = []
+array.length = 8
+
+// give us the size of the array
+array.length
+
+// position of items in the array
+// index
+//                0        1          2
+var superArr = ['rock', 'paper', 'scissors']
+//0 base, starts at zero
+
+//get a value with the index
+superArr[1]
+
+//insert in the array
+var supArr = []
+
+supArr.push('stan lee', 'spiderman')
+
+//it will add into the beginning of an array
+supArr.unshift(1, 2.3, true)
+
+
+//remove
+var sArr = [1,2,3]
+//removes the last element of the array and returns it
+sArr.pop
+//removes the first element of the array and returns it
+sArr.shift
